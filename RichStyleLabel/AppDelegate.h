6fe91a96f328a163df4fcab54264ef2dbe4099b1
liaojinxing <jinxingliao@gmail.com>
2014-06-01 22:20:56 +0800

    Initial Commit

---
+++ RichStyleLabel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RichStyleLabel
+//
+//  Created by liaojinxing on 14-6-1.
+//  Copyright (c) 2014年 jinxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RichStyleLabel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RichStyleLabel
+//
+//  Created by liaojinxing on 14-6-1.
+//  Copyright (c) 2014年 jinxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
